54e65188889a27e72b8d148b5c2f83e58ec49187
Kendall <kendallzamora2@gmail.com>
2025-04-27 23:07:37 -0600

    primer repositorio

---
+++ script.js
@@ -0,0 +1,23 @@
+const btnBase_2 = document.querySelector('#Base2');
+
+btnBase_2.addEventListener('click', () => {
+    window.location.href = 'Base_2.html';
+});
+
+const btnBase_8 = document.querySelector('#Base8');
+
+btnBase_8.addEventListener('click', () => {
+    window.location.href = 'Base_8.html';
+});
+
+const btnBase_10 = document.querySelector('#Base10');
+
+btnBase_10.addEventListener('click', () => {
+    window.location.href = 'Base_10.html';
+});
+
+const btnBase_16 = document.querySelector('#Base16');
+
+btnBase_16.addEventListener('click', () => {
+    window.location.href = 'Base_16.html';
+});
